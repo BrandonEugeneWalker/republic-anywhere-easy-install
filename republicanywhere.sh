44129e9afb639804aa0deebc597e654d516180c8
Brandon Walker <36686151+BrandonEugeneWalker@users.noreply.github.com>
2020-11-02 20:59:22 -0500

    Continued Debugging.
    Continued work on debugging the last line not working, as well as trying to remove unwanted output.

--- republicanywhere.sh
+++ republicanywhere.sh
@@ -5,8 +5,6 @@ forumurl="https://forums.republicwireless.com/t/beta-member-support-only-how-to-
 
 greeting="Welcome, this script will install $brand on Ubuntu and Debian distributions as well as it's dependencies. APT Warnings can be ignored.
 
-outputfile="republicscriptoutput.txt"
-
 The following actions will be performed:
  * Check for libgconf-2-4.
  * Install libgconf-2-4 if necessary.
@@ -42,19 +40,21 @@ then
 	else
 		echo "libgconf-2-4 is missing and will be installed."
 		echo "Installing libgconf-2-4..."
-		exec -c apt-get install -y libgconf-2-4 &> /dev/null
+		exec -c apt-get -qq install -y libgconf-2-4
 	fi
 	
 	echo "Fetching $brand .deb package..."
-	exec -c echo "deb [arch=amd64] https://s3.amazonaws.com/files.republicwireless.com/public/apps/anywhere/debian main main" | tee /etc/apt/sources.list.d/republicanywhere.list &> /dev/null
+	exec -c echo "deb [arch=amd64] https://s3.amazonaws.com/files.republicwireless.com/public/apps/anywhere/debian main main" | tee /etc/apt/sources.list.d/republicanywhere.list !> /dev/null
 	
 	echo "Adding $brand key to your keys..."
-	exec -c wget -O - https://s3.amazonaws.com/files.republicwireless.com/public/apps/anywhere/debian/key/public | apt-key add - &> /dev/null
+	exec -c wget -O - https://s3.amazonaws.com/files.republicwireless.com/public/apps/anywhere/debian/key/public | apt-key add -
 	
 	echo "Updating package list and installing $brand."
-	exec -c apt-get update & apt-get install -y republicanywhere &> /dev/null
+	exec -c apt-get -qq update 1> /dev/null & apt-get -qq install -y republicanywhere 1> /dev/null
 	
-	echo "Install completed..." "If $brand does not appear in your app menu restart your computer." "Exiting..."
+	echo "Install completed..." "If $brand does not appear in your app menu restart your computer."
+	echo "If you have any issue or questions the home of this script can be found at:"
+	echo "$forumurl"
 else
 	echo "Permission not granted terminating script."
 	exit 0
